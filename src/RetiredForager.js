e9fd9250369ad8b4a1e878b33c3f5babf2192f79
Abigail LI <palmigloo@gmail.com>
2022-10-31 10:47:36 -0700

    Complete BeesBeesBees

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,23 @@
-var RetiredForagerBee = function() {
+var RetiredForager = function() {
+  ForagerBee.call(this);
+
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+
 };
+
+RetiredForager.prototype = Object.create(ForagerBee.prototype);
+RetiredForager.prototype.constructor = RetiredForager;
+
+RetiredForager.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+}
+
+RetiredForager.prototype.gamble = function(treasure) {
+  this.forage(treasure);
+}
+
+
 
